0b201a94ea8da9d81b3698a1b615514297125160
Sandeep-svf <svf.sandeep@gmail.com>
2023-03-16 16:06:12 +0530

    sam

--- app/src/main/java/com/sam/alahmarkotlinclone/login/LoginActivity.kt
+++ app/src/main/java/com/sam/alahmarkotlinclone/login/LoginActivity.kt
@@ -19,6 +19,9 @@ import kotlinx.coroutines.launch
 class LoginActivity : AppCompatActivity() {
 
     lateinit var viewModel: LoginViewModel
+    lateinit var userEmail : AppCompatEditText
+    lateinit var userPassword : AppCompatEditText
+    lateinit var loginButton : AppCompatButton
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,47 +29,46 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
-        var userEmail = findViewById<AppCompatEditText>(R.id.user_email_edittext)
-        var userPassword = findViewById<AppCompatEditText>(R.id.uesr_password_edittext)
-        val loginButton = findViewById<AppCompatButton>(R.id.login_button)
+        userEmail = findViewById<AppCompatEditText>(R.id.user_email_edittext)
+        userPassword = findViewById<AppCompatEditText>(R.id.uesr_password_edittext)
+        loginButton = findViewById<AppCompatButton>(R.id.login_button)
 
 
         loginButton.setOnClickListener(View.OnClickListener {
-            if(validation (userEmail.text.toString(),userPassword.text.toString())){
+
+            Toast.makeText(this, "Login button clicked", Toast.LENGTH_SHORT).show()
+
+            if(validation ()){
+                viewModel.getLogin(userEmail.text.toString(),userPassword.text.toString(),"dskdjlfs132").observe(this, Observer {
+                    it.let {
+
+
+                        Log.d("test_sam",it.status)
+                        Log.d("test_sam",it.message)
+                        val token = it.token
+                        Log.d("test_sam",token.toString())
+
+                        SharedPreferenceUtil.saveAccessToken(token.access.toString())
+
+                        val accessToken = SharedPreferenceUtil.getAccessToken()
+
+                        Log.d("test_sam",accessToken.toString()+"ok")
+
+                        Toast.makeText(this, it.message, Toast.LENGTH_SHORT).show()
+
+                    }
+                })
 
             }
         })
-
-
-
-        viewModel.getLogin("sandeep@gmail.com","12345","dskdjlfs132").observe(this, Observer {
-            it.let {
-
-
-                    Log.d("test_sam",it.status)
-                    Log.d("test_sam",it.message)
-                    val token = it.token
-                    Log.d("test_sam",token.toString())
-
-                    SharedPreferenceUtil.saveAccessToken(token.access.toString())
-
-                    val accessToken = SharedPreferenceUtil.getAccessToken()
-
-                    Log.d("test_sam",accessToken.toString()+"ok")
-
-                    Toast.makeText(this, it.message, Toast.LENGTH_SHORT).show()
-
-            }
-        })
-
 
     }
 
-    private fun validation(userName : String, password : String): Boolean {
-        if(userName.equals("")){
+    private fun validation(): Boolean {
+        if(userEmail.text.toString().trim().equals("")){
             Toast.makeText(this, "Please enter user email",Toast.LENGTH_SHORT)
             return false
-        }else if(password.equals("")){
+        }else if(userPassword.text.toString().trim().equals("")){
             Toast.makeText(this, "Please enter user password",Toast.LENGTH_SHORT)
             return false
         }
